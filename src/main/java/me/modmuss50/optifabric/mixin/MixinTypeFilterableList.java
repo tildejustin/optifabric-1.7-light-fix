f0dccab6695c9ebc48124237861fa7fe6e0a7ba0
hYdos <haydenv06@gmail.com>
2020-04-21 14:24:19 +1000

    in game

---
+++ src/main/java/me/modmuss50/optifabric/mixin/MixinTypeFilterableList.java
@@ -0,0 +1,49 @@
+package me.modmuss50.optifabric.mixin;
+
+import com.google.common.collect.Iterators;
+import net.minecraft.util.TypeFilterableList;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+@Mixin(TypeFilterableList.class)
+public abstract class MixinTypeFilterableList<T> {
+
+
+    @Shadow @Final private Map<Class<?>, List<T>> elementsByType;
+
+    @Shadow protected abstract Class<?> method_6407(Class<?> var1);
+
+    @Shadow @Final private List<T> allElements;
+
+    /**
+     * @author hydos
+     */
+    @Overwrite
+    public <S> Iterable<S> method_6408(final Class<S> var1) {
+        return () -> {
+            List<T> list = elementsByType.get(method_6407(var1));
+            if (list == null) {
+                return Collections.emptyIterator();
+            } else {
+                Iterator<T> iterator = list.iterator();
+                return Iterators.filter(iterator, var1);
+            }
+        };
+    }
+
+    /**
+     * @author hydos
+     */
+    @Overwrite
+    public Iterator<T> iterator() {
+        return allElements.isEmpty() ? Collections.emptyIterator() : Iterators.unmodifiableIterator(allElements.iterator());
+    }
+
+}
